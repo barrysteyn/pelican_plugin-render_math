309be43b43ad44b9bc2363a40ef212c866addaa3
Barry Steyn <barry.steyn@gmail.com>
2012-12-31 10:49:39 -0500

    Added mathml rendering if available (consistent look on available browsers)

--- latex.py
+++ latex.py
@@ -5,53 +5,72 @@ Latex Plugin For Pelican
 
 This plugin allows you to write mathematical equations in your articles using Latex.
 It uses the MathJax Latex JavaScript library to render latex that is embedded in
-between $..$. It also allows for writing equations in by using \begin{equation}...
-\end{equation}.
+between `$..$` for inline math and `$$..$$` for displayed math. It also allows for 
+writing equations in by using `\begin{equation}`...`\end{equation}`.
 
 Settings
 --------
 To enable, ensure that the plugin is installed somewhere that is accessible.
-Then use as follows by adding:
-    
+Then use as follows by adding to the settings file:
+
     PLUGINS = ["latex"]
 
 to your settings.py
 
-Be careful: Not loading the plugin is eas to do, and difficult to detect. To
+Be careful: Not loading the plugin is easy to do, and difficult to detect. To
 make life easier, find where pelican is installed, and then copy the plugin
-there. Then do this:
+there. An easy way to find where pelican is installed is to verbose list the
+available themes by typing `pelican-themes -l -v`. 
+
+Once the pelican folder is found, copy plugin to the `plugins` folder. Then 
+add to settings file like this:
 
     PLUGINS = ["pelican.plugins.latex"]
 
 Usage
 -----
-Add the following to your template file between the <head> parameters (for 
+Add the following to your template file between the `<head>` parameters (for
 the NotMyIdea template, this file is base.html)
-    
+
     {% if article and article.latex %}
         {{ article.latex }}
     {% endif %}
 
 Now latex will be embedded in every article. If however you want latex only for
-selected articles, then in settings.py, add 
-    
+selected articles, then in settings.py, add
+
     LATEX = 'article'
 
-And in each article, add the metadata key 'latex'. For example, with the above 
-settings, creating an article called math, I would just include 'latex' as 
-part of the metadata:
+And in each article, add the metadata key `latex:`. For example, with the above
+settings, creating an article that I want to render latex math, I would just 
+include 'Latex' as part of the metadata without any value:
 
     Date: 1 sep 2012
     Status: draft
     Latex:
+
+Latex Examples
+--------------
+###Inline
+Latex between `$`..`$`, for example, `$`x^2`$`, will be rendered inline with respect to the current html block.
+
+###Displayed Math
+Latex between `$$`..`$$`, for example, `$$`x^2`$$`, will be rendered centered in a new paragraph.
+
+###Equations
+Latex between `\begin` and `\end`, for example, `begin{equation}` x^2 `\end{equation}`, will be rendered centered in a new paragraph with a right justified equation number at the top of the paragraph. This equation number can be referenced in the document. To do this, use a `label` inside of the equation format and then refer to that label using `ref`. For example: `begin{equation}` `\label{eq}` X^2 `\end{equation}`. Now refer to that equation number by `$`\ref{eq}`$`.
+   
+Template And Article Examples
+-----------------------------
+To see an example of this plugin in action, look at [this article](http://doctrina.org/How-RSA-Works-With-Examples.html). To see how this plugin works with a template, look at [this template](https://github.com/barrysteyn/pelican_theme-personal_blog).
 """
 
 from pelican import signals
 
 latexScript = '\n\
-    <script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type= "text/javascript">\n\
+    <script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type= "text/javascript"></script>\n\
+    <script>\n\
        MathJax.Hub.Config({\n\
-           HTML: ["input/TeX","output/HTML-CSS"],\n\
            TeX: { extensions: ["AMSmath.js","AMSsymbols.js"], equationNumbers: { autoNumber: "AMS" } },\n\
            extensions: ["tex2jax.js"],\n\
            jax: ["input/TeX","output/HTML-CSS"],\n\
